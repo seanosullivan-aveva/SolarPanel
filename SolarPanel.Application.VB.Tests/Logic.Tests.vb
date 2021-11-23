61b09eab4724bcd2dc9bc5ceb56af9d1f44cafd1
Sean O'Sullivan <sean.osullivan@aveva.com>
2021-11-23 16:25:34 +0000

    Add some simple vb unit tests to test the house utilities class

---
+++ SolarPanel.Application.VB.Tests/Logic.Tests.vb
@@ -0,0 +1,24 @@
+Imports System
+Imports System.Drawing
+Imports NUnit.Framework
+Imports SolarPanel.Logic.Utilities
+Imports SolarPanel.Types
+
+<TestFixture>
+Public Class HouseUtilities_Tests
+
+    <TestCase(4.0F, 2.0F, 1.0F, 1.0F, ExpectedResult:=8)>
+    <TestCase(4.0F, 2.0F, 1.1F, 1.1F, ExpectedResult:=3)>
+    <TestCase(4.0F, 2.0F, 0.5F, 0.5F, ExpectedResult:=32)>
+    Function House_Utilities_Panels(roofWidth, roofHeight, panelHeight, panelWidth)
+        ' Arrange
+        Dim house = New House("test", New SizeF(roofWidth, roofHeight), 0F, 0F)
+        Dim panel = New Panel("", "test", New SizeF(panelHeight, panelWidth), 0F, 0F, 0F, 0F, String.Empty)
+
+        ' Act
+        Dim result = HouseUtilities.MaxNumberOfPanels(house, panel)
+
+        ' Assert
+        Return result
+    End Function
+End Class
