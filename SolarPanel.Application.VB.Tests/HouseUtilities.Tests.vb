8dc894558caf2a761aeac3519f2f48e88e2b1d58
Sean O'Sullivan <sean.osullivan@aveva.com>
2021-11-24 09:30:59 +0000

    Add VB unit test to cover tariff utilities

--- SolarPanel.Application.VB.Tests/HouseUtilities.Tests.vb
+++ SolarPanel.Application.VB.Tests/HouseUtilities.Tests.vb
@@ -10,7 +10,7 @@ Public Class HouseUtilities_Tests
     <TestCase(4.0F, 2.0F, 1.0F, 1.0F, ExpectedResult:=8)>
     <TestCase(4.0F, 2.0F, 1.1F, 1.1F, ExpectedResult:=3)>
     <TestCase(4.0F, 2.0F, 0.5F, 0.5F, ExpectedResult:=32)>
-    Function House_Utilities_Panels(roofWidth, roofHeight, panelHeight, panelWidth)
+    Function HouseUtilities_Panels(roofWidth, roofHeight, panelHeight, panelWidth)
         ' Arrange
         Dim house = New House("test", New SizeF(roofWidth, roofHeight), 0F, 0F)
         Dim panel = New Panel("", "test", New SizeF(panelHeight, panelWidth), 0F, 0F, 0F, 0F, String.Empty)
---
+++ SolarPanel.Application.VB.Tests/TariffUtilities.Tests.vb
@@ -0,0 +1,21 @@
+Imports System
+Imports System.Drawing
+Imports NUnit.Framework
+Imports SolarPanel.Logic.Utilities
+Imports SolarPanel.Types
+
+<TestFixture>
+Public Class TariffUtilities_Tests
+
+    <TestCase(1.0F, 1.0F, 8.0F, ExpectedResult:=0.00008)>
+    Function TariffUtilities_Panels(price, power, hours)
+        ' Arrange
+        Dim tariff = New Tariff("test", price, DateTime.MaxValue, 0F, vbNull, vbNull)
+
+        ' Act
+        Dim result = TariffUtilities.ComputeAmountPaid(tariff, power, New Date(2021, 1, 1), TimeSpan.FromHours(hours))
+
+        ' Assert
+        Return result
+    End Function
+End Class
